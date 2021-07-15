e7f9d33a458767ba470e331f68c109d86be5d8ec
Marcos Cunha Lima <marcos@futuranet.com.br>
2021-07-15 17:57:08 -0300

    StatusBar OK

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,57 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+
+const parametersWrapper = {
+   large: {
+      height: '24px',
+      borderRadius: '3px',
+      padding: '4px'
+   },
+   medium: {
+      height: '12px',
+      borderRadius: '3px',
+      padding: 0
+   },
+   small: {
+      height: '8px',
+      borderRadius: '3px',
+      padding: 0
+   },
+}
+
+const getInnerProps = (props, value) => {
+   const propsInner = {}
+   const borderRadius = value === 100 ? props.borderRadius : '0';
+   propsInner['innerRadius'] = props.borderRadius + ' ' + borderRadius + ' ' +
+      borderRadius + ' ' + props.borderRadius
+   propsInner['width'] = value + '%'
+   return propsInner;
+}
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+
+   const props = size === 'large' ? parametersWrapper['large'] : (size === 'medium' ?
+      parametersWrapper['medium'] : parametersWrapper['small']);
+
+   return <Wrapper {...props}> <InnerProgressBar {...getInnerProps(props, value)} /></Wrapper>;
 };
+
+const Wrapper = styled.div`
+   width: 100%;
+   background-color: ${COLORS.gray50};
+   box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+   height: ${props => props.height};
+   border-radius: ${props => props.borderRadius};
+   padding: ${props => props.padding};
+`;
+
+
+const InnerProgressBar = styled.div`
+   background-color:${COLORS.primary};
+   width: ${props => props.width};
+   height: 100%;
+   border-radius: ${props => props.innerRadius} ;
+`;
 
 export default ProgressBar;
--- src/constants.js
+++ src/constants.js
@@ -7,5 +7,4 @@ export const COLORS = {
    black: 'hsl(0deg 0% 0%)',
    transparentGray15: 'hsl(0deg 0% 50% / 0.15)',
    transparentGray35: 'hsl(0deg 0% 50% / 0.35)',
-   transparentGray50: 'hsl(0deg 0% 50% / 0.50)',
 };

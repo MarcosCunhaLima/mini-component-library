64fb46cc02e733e541b74bb08fed0ac8f140c19e
Marcos Cunha Lima <marcos@futuranet.com.br>
2021-07-19 11:29:47 -0300

    finished

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -7,20 +7,20 @@ import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
 const parametersInput = {
-  small: {
-    border: '1px',
-    fontSize: '14px',
-    iconSize: 16,
-    paddingIcon: 24,
-    padding: 4
-  },
-  large: {
-    border: '2px',
-    fontSize: '18px',
-    iconSize: 24,
-    paddingIcon: 32,
-    padding: 8
-  }
+   small: {
+      border: '1px',
+      fontSize: '14px',
+      iconSize: 16,
+      paddingIcon: 24,
+      padding: 4
+   },
+   large: {
+      border: '2px',
+      fontSize: '18px',
+      iconSize: 24,
+      paddingIcon: 32,
+      padding: 8
+   }
 }
 
 
@@ -39,13 +39,15 @@ const InnerInput = styled.input`
   &:focus {
     outline-offset: 2px;
   };
+  color: inherit;
+`;
+
+const Wrapper = styled.label`
+  position: relative;
+  color: ${COLORS.gray700};
   &:hover {
     color: ${COLORS.black};
   }
-`;
-
-const Wrapper = styled.div`
-  position: relative;
   
 `;
 
@@ -54,22 +56,25 @@ const InnerIcon = styled(Icon)`
   top: 0;
   bottom: 0;
   margin: auto;
+  
+  
 `;
 
 const IconInput = ({
-  label,
-  icon,
-  width = 250,
-  size,
-  placeholder,
+   label,
+   icon,
+   width = 250,
+   size,
+   placeholder,
 }) => {
-  const parInput = parametersInput[size]
-  return (
-    <Wrapper>
-      <InnerIcon id={icon} size={parInput.iconSize} />
-      <InnerInput {...parInput} placeholder={placeholder} width={width + 'px'} />
-    </Wrapper>
-  );
+   const parInput = parametersInput[size]
+   return (
+      <Wrapper>
+         <VisuallyHidden>{label}</VisuallyHidden>
+         <InnerIcon id={icon} size={parInput.iconSize} />
+         <InnerInput {...parInput} placeholder={placeholder} width={width + 'px'} />
+      </Wrapper>
+   );
 };
 
 export default IconInput;
